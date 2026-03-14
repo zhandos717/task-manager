71c329b21ef536280838da6f29ccd28a27a35743
Zhandos <zhandos.zhandarbekov@gmail.com>
2026-03-14 10:52:26 +0500

    Initial Commit

---
+++ TaskManager/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  TaskManager
+//
+//  Created by Zhandos Zhandarbek on 14.03.2026.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
---
+++ TaskManagerTests/TaskManagerTests.swift
@@ -0,0 +1,17 @@
+//
+//  TaskManagerTests.swift
+//  TaskManagerTests
+//
+//  Created by Zhandos Zhandarbek on 14.03.2026.
+//
+
+import Testing
+@testable import TaskManager
+
+struct TaskManagerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
